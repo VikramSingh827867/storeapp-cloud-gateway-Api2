9a2797c2ffacbd6702fd1c0b01a703cad5fedd1b
Vikram Singh <singhvks30@gmail.com>
2024-10-09 10:58:31 +0530

    API

---
+++ src/test/java/com/mphasis/storeapp_cloud_gateway_api/StoreappCloudGatewayApi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mphasis.storeapp_cloud_gateway_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreappCloudGatewayApi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
